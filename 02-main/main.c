dad985e18f8f274e7a8d7538301b81156a00d7f3
Aaron Tan <hi@aaronhktan.com>
2020-08-22 02:45:13 -0400

    Port to F446RE
    - F446RE has many more interrupts, so needed to significantly modify the vector table.
    - Also added a static variable initialized to 0 and static variable initialized to 0xDEADBEEF to show data in ELF between _sbss/_ebss and _sdata/_edata respectively.

--- 02-main/main.c
+++ 02-main/main.c
@@ -1,6 +1,14 @@
+// This should show up as 4 bytes between _sbss and _ebss
+static int val_s = 0;
+// This should show up as 4 bytes between _sdata and _edata
+static volatile int other_val = 0xDEADBEEF;
+
 int main(void) {
   int val = 0;
+
   while (1) {
+    val_s += 2;
     val += 1;
+    other_val = 0xDEADBEEF;
   }
 }
